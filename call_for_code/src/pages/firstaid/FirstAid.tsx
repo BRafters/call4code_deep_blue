40f358605e8ec0335ab7c2977ac325895d6f6030
midgriffin <43070418+midgriffin@users.noreply.github.com>
2020-06-17 19:22:37 -0300

    Json in, displaying procedure names in list, attempting to click event

--- call_for_code/src/pages/firstaid/FirstAid.tsx
+++ call_for_code/src/pages/firstaid/FirstAid.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, 
+     IonContent, 
+     IonHeader, 
+     IonMenuButton, 
+     IonPage, 
+     IonTitle, 
+     IonToolbar, 
+     IonCard,
+     IonCardHeader,
+     IonCardSubtitle,
+     IonCardTitle,
+     IonCardContent,
+     IonList,
+     IonItem } from '@ionic/react';
+import data from './firstAidData.json';
 import './FirstAid.css';
+ 
 
 const FirstAid: React.FC = () => {
+
      const { name } = useParams<{name: string;}>();
      return(
           <IonPage>
@@ -12,16 +28,38 @@ const FirstAid: React.FC = () => {
                <IonButtons slot="start">
                <IonMenuButton />
                </IonButtons>
-               <IonTitle>This is the First Aid Page</IonTitle>
+               <IonTitle>Emergency First Aid Procedures</IonTitle>
           </IonToolbar>
           </IonHeader>
-
           <IonContent>
           <IonHeader collapse="condense">
                <IonToolbar>
                <IonTitle size="large">{name}</IonTitle>
                </IonToolbar>
           </IonHeader>
+               <IonList>
+                    {
+                         data.map((Procedure, index)=>{
+                              return (
+                                   <IonItem onClick={() => {
+                                        data.filter(procedures => procedures.id === index );
+                                        return (<IonCard>
+                                             <IonCardHeader>
+                                                  <IonCardTitle>{Procedure.procedure}</IonCardTitle>
+                                             </IonCardHeader>
+                                             <IonCardContent>
+                                                  <IonList>
+                                                       {Procedure.steps}
+                                                  </IonList>
+                                             </IonCardContent>
+                                         </IonCard>      )
+                                   }}>
+                                             {Procedure.procedure}
+                                   </IonItem>
+                              )
+                         })
+                    }
+               </IonList>
           </IonContent>
 
           
