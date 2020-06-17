e74f660c0943764f35761e51c475baa5ecac3513
BRafters <brennanrafters@gmail.com>
2020-06-17 11:32:38 -0300

    Fixed scrolling

--- call_for_code/src/pages/firstaid/FirstAid.tsx
+++ call_for_code/src/pages/firstaid/FirstAid.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router';
-import { IonButtons, IonContent, IonHeader, IonIcon, IonMenuButton, IonPage, IonTitle, IonToolbar, IonNav, IonItem, IonLabel } from '@ionic/react';
+import { IonButtons, IonContent, IonHeader, IonIcon, IonMenuButton, IonPage, IonTitle, IonToolbar, IonNav, IonItem, IonLabel,
+      IonList, IonVirtualScroll } from '@ionic/react';
 import { sadOutline, compassOutline, fitnessOutline, heartOutline, personOutline, restaurantOutline, roseOutline, snowOutline, walkOutline } from 'ionicons/icons';
 import './FirstAid.css';
 
@@ -16,46 +17,43 @@ const FirstAid: React.FC = () => {
                <IonTitle>This is the First Aid Page</IonTitle>
           </IonToolbar>
           </IonHeader>
-
           <IonContent>
-               <IonNav>
-                    <IonItem button >
-                         <IonIcon icon={sadOutline} className="health"></IonIcon>
-                         <IonLabel className="fonts">Test link</IonLabel>
-                    </IonItem>
-                    <IonItem button >
-                         <IonIcon icon={compassOutline} className="health"></IonIcon>
-                         <IonLabel className="fonts">Test link</IonLabel>
-                    </IonItem>
-                    <IonItem button >
-                         <IonIcon icon={fitnessOutline} className="health"></IonIcon>    
-                         <IonLabel className="fonts">Test link</IonLabel>
-                    </IonItem>
-                    <IonItem button >
-                         <IonIcon icon={heartOutline} className="health"></IonIcon>
-                         <IonLabel className="fonts">Test link</IonLabel>
-                    </IonItem>
-                    <IonItem button >
-                         <IonIcon icon={personOutline} className="health"></IonIcon>
-                         <IonLabel className="fonts">Test link</IonLabel>
-                    </IonItem>
-                    <IonItem button >
-                         <IonIcon icon={restaurantOutline} className="health"></IonIcon>
-                         <IonLabel className="fonts">Test link</IonLabel>
-                    </IonItem>
-                    <IonItem button >
-                         <IonIcon icon={roseOutline} className="health"></IonIcon>
-                         <IonLabel className="fonts">Test link</IonLabel>
-                    </IonItem> 
-                    <IonItem button >
-                         <IonIcon icon={snowOutline} className="health"></IonIcon>
-                         <IonLabel className="fonts">Test link</IonLabel>
-                    </IonItem>
-                    <IonItem button >
-                         <IonIcon icon={walkOutline} className="health"></IonIcon>
-                         <IonLabel className="fonts">Test link</IonLabel>
-                    </IonItem>
-               </IonNav>
+               <IonItem button >
+                    <IonIcon icon={sadOutline} className="health"></IonIcon>
+                    <IonLabel className="fonts">Test link</IonLabel>
+               </IonItem>
+               <IonItem button >
+                    <IonIcon icon={compassOutline} className="health"></IonIcon>
+                    <IonLabel className="fonts">Test link</IonLabel>
+               </IonItem>
+               <IonItem button >
+                    <IonIcon icon={fitnessOutline} className="health"></IonIcon>    
+                    <IonLabel className="fonts">Test link</IonLabel>
+               </IonItem>
+               <IonItem button >
+                    <IonIcon icon={heartOutline} className="health"></IonIcon>
+                    <IonLabel className="fonts">Test link</IonLabel>
+               </IonItem>
+               <IonItem button >
+                    <IonIcon icon={personOutline} className="health"></IonIcon>
+                    <IonLabel className="fonts">Test link</IonLabel>
+               </IonItem>
+               <IonItem button >
+                    <IonIcon icon={restaurantOutline} className="health"></IonIcon>
+                    <IonLabel className="fonts">Test link</IonLabel>
+               </IonItem>
+               <IonItem button >
+                    <IonIcon icon={roseOutline} className="health"></IonIcon>
+                    <IonLabel className="fonts">Test link</IonLabel>
+               </IonItem> 
+               <IonItem button >
+                    <IonIcon icon={snowOutline} className="health"></IonIcon>
+                    <IonLabel className="fonts">Test link</IonLabel>
+               </IonItem>
+               <IonItem button >
+                    <IonIcon icon={walkOutline} className="health"></IonIcon>
+                    <IonLabel className="fonts">Test link</IonLabel>
+               </IonItem>
           </IonContent> 
      </IonPage>
      );
