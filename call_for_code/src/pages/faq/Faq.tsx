a0b50f35085c861ae6cc818e73b72f68d2c6ac08
sam.davey888@gmail.com <sam.davey888@gmail.com>
2020-06-18 09:25:56 -0300

    Added stuff (FAQ)
    Yes

--- call_for_code/src/pages/faq/Faq.tsx
+++ call_for_code/src/pages/faq/Faq.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useParams } from 'react-router';
+import { IonButtons, IonContent, IonHeader, IonIcon, IonMenuButton, IonPage, IonTitle, IonToolbar, IonNav, IonItem, IonLabel, IonList } from '@ionic/react';
+import { sadOutline, compassOutline, fitnessOutline, heartOutline, personOutline, restaurantOutline, roseOutline, snowOutline, walkOutline, helpOutline } from 'ionicons/icons';
 import './Faq.css';
 
 const Faq: React.FC = () => {
-     
+
+     const items = [
+          {
+               question: "Does this thing work?",
+               answer: "Yes"
+          }
+     ]
+
      return(
-          <IonPage>
+     <IonPage>
           <IonHeader>
           <IonToolbar color="primary">
                <IonButtons slot="start">
                <IonMenuButton />
                </IonButtons>
-               <IonTitle>Faq</IonTitle>
+               <IonTitle>FAQ</IonTitle>
           </IonToolbar>
           </IonHeader>
 
           <IonContent>
-            FAQ Page
-          </IonContent>
 
-
+               {
+                    items.map((item, key) => { return(
+                         <IonItem button key={key}>
+                              <IonIcon icon={helpOutline} color="dark"></IonIcon>
+                              <IonLabel className="fonts"> {item.question} </IonLabel>
+                         </IonItem> 
+                    )})
+               }
+     
+          </IonContent> 
      </IonPage>
      );
 }
