752ccc522e4ba5a573329b4c6e7dd7941ee68152
midgriffin <43070418+midgriffin@users.noreply.github.com>
2020-06-19 16:25:18 -0300

    first aid selected displaying steps

--- call_for_code/src/pages/firstaid/FirstAidSelected.tsx
+++ call_for_code/src/pages/firstaid/FirstAidSelected.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router';
-import { IonButtons, IonContent, IonHeader, IonIcon, IonMenuButton, IonPage, IonTitle, IonToolbar, IonNav, IonItem, IonLabel, IonList, IonBackButton, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent} from '@ionic/react';
+import { IonButtons, IonContent, IonHeader, IonIcon, IonMenuButton, IonPage, IonTitle, IonToolbar, IonNav, IonItem, IonLabel, IonList, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonBackButton } from '@ionic/react';
 import data from '../../data.json';
 import './FirstAid.css';
 
@@ -10,9 +10,6 @@ const FirstAidSelected: React.FC = () => {
     const firstAidData = data[0].firstaid;
 
     const item = firstAidData.filter(FAProcedure => FAProcedure.id === parseInt(id));
-    
-
-    
 
      return(
      <IonPage>
@@ -24,26 +21,25 @@ const FirstAidSelected: React.FC = () => {
                <IonButtons slot="end">
                <IonMenuButton />
                </IonButtons>
-               <IonTitle>{item[0].procedure}</IonTitle>
+               <IonTitle> {item[0].procedure} </IonTitle>
                <IonCard>
-            <IonCardHeader>
-                <IonCardTitle>Care steps</IonCardTitle>
-            </IonCardHeader>
-            <IonCardContent>
-              <IonList>
-              {
-                item[0].steps.map((step, index) => {
-                  return (
-                      <IonItem key={index}>
-                        {step}
-                      </IonItem>
-                  )
-                })
-
-              }
-              </IonList>
-            </IonCardContent>
-        </IonCard>
+                    <IonCardHeader>
+                         <IonCardTitle>Care Steps</IonCardTitle>
+                    </IonCardHeader>
+                    <IonCardContent>
+                         <IonList>
+                              {
+                                   item[0].steps.map((step, index) => {
+                                        return(
+                                             <IonItem key={index}>
+                                                  {step}
+                                             </IonItem>
+                                        )
+                                   })
+                              }
+                         </IonList>
+                    </IonCardContent>    
+               </IonCard>
           </IonToolbar>
           </IonHeader>
 
