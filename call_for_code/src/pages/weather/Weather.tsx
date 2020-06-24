c704c34b08c704673ffeaadfba6267f89fd4b54c
BRafters <brennanrafters@gmail.com>
2020-06-24 10:02:37 -0300

    Revert "Weather Update"
    This reverts commit 787fb47d32cab7e1ba938af6b787dcbac9a94a89.

--- call_for_code/src/pages/weather/Weather.tsx
+++ call_for_code/src/pages/weather/Weather.tsx
@@ -1,35 +1,10 @@
 import React, { useState, useEffect, Component } from 'react';
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonToolbar, IonFab, IonFabButton, IonIcon } from '@ionic/react';
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonToolbar } from '@ionic/react';
 import './Weather.css';
 import WeatherData from './weather_data.js';
 import CreateMap from './maps.js';
-import { arrowUp, arrowDown } from 'ionicons/icons';
 
 const Weather: React.FC = () => {
-     let overlayOn = true;
-
-     function moveOverlay() {
-          let overlay = document.getElementById("home__overlay");
-          let background = document.getElementById("background-circle");
-          let button = document.getElementById("showOverlayBtn");
-
-          if (overlay != null && background != null && button != null) {
-               if (overlayOn) {
-                    overlay.style.top = "-100vh";
-                    background.style.top = "-100vh";
-                    button.style.top = "1vh";
-                    button.style.transform = "rotate(180deg)"
-                    overlayOn = false;
-               } else {
-                    overlay.style.top = "-10vh";
-                    background.style.top = "-25vh";
-                    button.style.top = "58vh";
-                    button.style.transform = "rotate(0deg)"
-                    overlayOn = true;
-               }
-          }
-     }
-
      return(
           <IonPage>
           <IonHeader>
@@ -43,21 +18,14 @@ const Weather: React.FC = () => {
           <IonContent>
                <div id="background-circle"></div>
                <section id="home__overlay">
-                    <WeatherData /> 
+                    <WeatherData />
                </section>
-               <IonFab id="showOverlayBtn" horizontal="center" slot="fixed">
-                    <IonFabButton color="primary" onMouseDown={moveOverlay}> 
-                         <IonIcon id="btnHideOverlay" icon={arrowUp} />
-                    </IonFabButton>
-               </IonFab>
-               
                <section id="map_overlay">
                     <CreateMap />
                </section>
           </IonContent>
      </IonPage>
      );
-     
 }
 
 export default Weather;
